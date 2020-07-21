c7e210296c465034c922d256aee78b93123ddf96
tian <601814578@qq.com>
2020-07-21 14:31:57 +0800

    使用 setting 中的数据库配置连接数据库，测试成功

--- db/base_class.py
+++ db/base_class.py
@@ -6,8 +6,8 @@ from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:tian@localhost:5432/fastapi"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
