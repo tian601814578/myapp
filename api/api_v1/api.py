1dbc1a3bf9657ea00b44ab34246e1590c2ac54be
tian <601814578@qq.com>
2020-07-22 15:24:54 +0800

    添加接口模块--角色，测试成功

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -5,4 +5,5 @@ from api.api_v1.endpoints import items, users, role
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+# 角色
 api_router.include_router(role.router, prefix="/role", tags=["role"])
